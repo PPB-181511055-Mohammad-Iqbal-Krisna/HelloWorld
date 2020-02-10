7bae92b92949e92a374a5b72fa0e035e3c25b330
kresnaiqbal <mohammad.iqbal.krisna.tif18@polban.ac.id>
2020-02-10 21:11:37 +0700

    first commit

---
+++ app/src/main/java/com/example/acer/helloiqbal/HelloIqbal.java
@@ -0,0 +1,13 @@
+package com.example.acer.helloiqbal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloIqbal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_iqbal);
+    }
+}
